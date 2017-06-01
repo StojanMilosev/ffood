083afb94ab64b48db2013402b4b923d3818a8324
stojan.milosev <stojan.milosev@yahoo.com>
2017-06-01 21:45:20 +0200

    added config file

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+    return (object) array(
+        'db_host'=>'ec2-54-246-108-119.eu-west-1.compute.amazonaws.com',
+        'db_name'=>'d3h563fo9fe024',
+        'db_username'=>'iwqbnyeyicgusi',
+        'db_password'=>'2bc81489f1d7e68ad0acaff16bd82354a3ba863586ffccca8dc10ac95bed8fc4',
+        'smtp_host'=>'smtp.mailtrap.io',
+        'smtp_port'=>'2525',
+        'smtp_username'=>'a032821f736fbf',
+        'smtp_password'=>'4add96c8584bf5',
+    );
+?>
