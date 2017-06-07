ec39bf8a89edb7420ce01d57501221c2c2006c82
stojan.milosev <stojan.milosev@yahoo.com>
2017-06-07 19:09:40 +0200

    config CHANGES

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,7 @@
 <?php
+    $dbout = parse_url(getenv('DATABASE_URL'));
     return (object) array(
-        'db_host'=>getenv('DB_HOST),
+        'db_host'=>$dbout["host"],
         'db_name'=>'heroku_a8fa9b4847b6257',
         'db_username'=>'b08f00a8f7b126',
         'db_password'=>'ddb49660',
