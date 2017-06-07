3cbedc95ca6463a458382acb54ac7a6206c08cd8
stojan.milosev <stojan.milosev@yahoo.com>
2017-06-07 19:05:37 +0200

    config CHANGES

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
     return (object) array(
-        'db_host'=>'DB_HOST',
+        'db_host'=>getenv('DB_HOST),
         'db_name'=>'heroku_a8fa9b4847b6257',
         'db_username'=>'b08f00a8f7b126',
         'db_password'=>'ddb49660',
