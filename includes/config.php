0e7fa01ff47fca8c0c26be7a05ff5972d7db2640
stojan.milosev <stojan.milosev@yahoo.com>
2017-06-09 14:31:11 +0200

    added smtp credentials

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,23 @@
 <?php
-    $host = getenv('DB_HOST');
-    $name = getenv('DB_NAME');
-    $user = getenv('DB_USER');
-    $pass = getenv('DB_PASS');
+    //databse credentials
+    $db_host = getenv('DB_HOST');
+    $db_name = getenv('DB_NAME');
+    $db_user = getenv('DB_USER');
+    $db_pass = getenv('DB_PASS');
+    //smtp credentials
+    $smtp_host = getenv('SMTP_HOST');
+    $smtp_name = getenv('SMTP_NAME');
+    $smtp_port = getenv('SMTP_PORT');
+    $smtp_user = getenv('SMTP_USER');
+    $smtp_pass = getenv('SMTP_PASS');
     return (object) array(
-        'db_host'=>$host,
-        'db_name'=>$name,
-        'db_username'=>$user,
-        'db_password'=>$pass,
-        'smtp_host'=>'',
-        'smtp_port'=>'',
-        'smtp_username'=>'',
-        'smtp_password'=>'',
+        'db_host'=>$db_host,
+        'db_name'=>$db_namee,
+        'db_username'=>$db_user,
+        'db_password'=>$db_pass,
+        'smtp_host'=>$smtp_host,
+        'smtp_port'=>$smtp_port,
+        'smtp_username'=>$smpt_name,
+        'smtp_password'=>$smtp_pass,
     );
 ?>
